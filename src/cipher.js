b5d5fca3a9014aa5b9e92502e8dc358798368192
Batistadriana <batistadriana@hotmail.com>
2019-02-05 11:53:20 -0600

    Codigo cipher
      -Hacer y probar el código para 'Decode'.
      -Hacer y probar el código para 'Encode'.
      -Meter el código en el objeto 'window.cipher'.

--- src/cipher.js
+++ src/cipher.js
@@ -13,8 +13,21 @@ encode: (offset, texto)=> {
     }
     let x = mensaje.join('');
     return x;
-  }
+  },
 
+  decode: (offset, texto)=> {
 
+      let mensaje = [];
 
-}
+      for(let i=0; i < texto.length; i++){
+          texto[i].charCodeAt();
+          let position = ((texto[i].charCodeAt() + 65 - offset)%26 + 65);
+          let asciiString= String.fromCharCode(position);
+          mensaje.push(asciiString);
+
+      }
+      let x = mensaje.join('');
+      return x;
+    }
+
+};
