7e631210b7162b7604f3aa7ede3a49b3f7438205
Batistadriana <batistadriana@hotmail.com>
2019-02-05 18:40:23 -0600

    Cambios index.js
      -Agregar funciones para los botones de decode y encode.
      -Hacer que los botones funcionen (decode y encode).

--- src/index.js
+++ src/index.js
@@ -1,33 +1,51 @@
+// //Función para prender y apagar de ventanas
+// function restart(){
+//
+//   document.getElementById("screen2a").style.display = 'none';
+//   document.getElementById("screen2b").style.display = 'none';
+//   document.getElementById("screen3a").style.display = 'none';
+//   document.getElementById("screen3b").style.display = 'none';
+//
+// } restart();
+//
+// function controlScreens(divX, divY){
+//  let divElement = document.getElementById(divX);
+//  let divElement2 = document.getElementById(divY);
+//
+//  // apagar pantalla 1
+//  if(divElement.style.display == 'none'){
+//    divElement.style.display = 'block';
+//  }
+//  else {
+//    divElement.style.display = 'none';
+//  }
+//
+//  // prender pantallas
+//  if(divElement2.style.display == 'none'){
+//    divElement2.style.display = 'block';
+//  }
+//  else {
+//    divElement2.style.display = 'none';
+//  }
+// }
 
+//Funciones para los argumentos de cipher.js
+function encodeArg(){
 
+    let offsetIn = Number(document.getElementById("offsetC").value);
+    let messageIn = document.getElementById("messageC").value;
+    let messageCod = window.cipher.encode(offsetIn, messageIn);
+    document.getElementById('outputE').innerHTML = messageCod;
 
-//Funcion para prender y apagar de ventanas
-function restart(){
+  }
+   document.getElementById('encode').addEventListener('click', encodeArg);
 
-  document.getElementById("screen2a").style.display = 'none';
-  document.getElementById("screen2b").style.display = 'none';
-  document.getElementById("screen3a").style.display = 'none';
-  document.getElementById("screen3b").style.display = 'none';
+function decodeArg(){
 
-} restart();
+  let offsetIn = Number(document.getElementById("offsetD").value);
+  let messageIn = document.getElementById("messageD").value;
+  let messageDec = window.cipher.decode(offsetIn, messageIn);
+  document.getElementById('outputD').innerHTML = messageDec;
 
-function controlScreens(divX, divY){
- let divElement = document.getElementById(divX);
- let divElement2 = document.getElementById(divY);
-
- // apagar pantalla 1
- if(divElement.style.display == 'none'){
-   divElement.style.display = 'block';
- }
- else {
-   divElement.style.display = 'none';
- }
-
- // prender pantallas
- if(divElement2.style.display == 'none'){
-   divElement2.style.display = 'block';
- }
- else {
-   divElement2.style.display = 'none';
- }
-}
+  }
+  document.getElementById('decode').addEventListener('click', decodeArg);
