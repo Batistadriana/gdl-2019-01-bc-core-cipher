cb58746e9a3cc282dd79a18773881461582151de
Batistadriana <batistadriana@hotmail.com>
2019-02-07 08:40:50 -0600

    Agregué función para cambiar pantallas.
      -Hice una función para cambiar a la pantalla de cifrar.
      -Hice una función para cambiar a la pantalla de descifrar.
      -Hice una función para cambiar a la pantalla principal desde cifrar.
      -Hice una función para cambiar a la pantalla principal desde
      descifrar.

--- src/index.js
+++ src/index.js
@@ -1,51 +1,49 @@
-// //Función para prender y apagar de ventanas
-// function restart(){
-//
-//   document.getElementById("screen2a").style.display = 'none';
-//   document.getElementById("screen2b").style.display = 'none';
-//   document.getElementById("screen3a").style.display = 'none';
-//   document.getElementById("screen3b").style.display = 'none';
-//
-// } restart();
-//
-// function controlScreens(divX, divY){
-//  let divElement = document.getElementById(divX);
-//  let divElement2 = document.getElementById(divY);
-//
-//  // apagar pantalla 1
-//  if(divElement.style.display == 'none'){
-//    divElement.style.display = 'block';
-//  }
-//  else {
-//    divElement.style.display = 'none';
-//  }
-//
-//  // prender pantallas
-//  if(divElement2.style.display == 'none'){
-//    divElement2.style.display = 'block';
-//  }
-//  else {
-//    divElement2.style.display = 'none';
-//  }
-// }
+ function boton2a() {
+     document.getElementById("screen1").style.display = "none";
+     document.getElementById("screen2a").style.display = "block";
+  }
+     document.getElementById('botonEncode').addEventListener('click', boton2a);
+
+ function boton2b(){
+     document.getElementById('screen1').style.display = "none";
+     document.getElementById("screen2b").style.display = "block";
+  }
+     document.getElementById('botonDecode').addEventListener('click', boton2b);
+
+ function returnA(){
+     document.getElementById("screen2a").style.display = "none";
+     document.getElementById("screen1").style.display = "block";
+  }
+
+     document.getElementById('volverA').addEventListener('click', returnA);
+
+ function returnB(){
+     document.getElementById("screen2b").style.display = "none";
+     document.getElementById("screen1").style.display = "block";
+  }
+
+     document.getElementById('volverB').addEventListener('click', returnB);
+
 
 //Funciones para los argumentos de cipher.js
-function encodeArg(){
+ function encodeArg(){
 
-    let offsetIn = Number(document.getElementById("offsetC").value);
-    let messageIn = document.getElementById("messageC").value;
-    let messageCod = window.cipher.encode(offsetIn, messageIn);
-    document.getElementById('outputE').innerHTML = messageCod;
-
-  }
-   document.getElementById('encode').addEventListener('click', encodeArg);
-
-function decodeArg(){
-
-  let offsetIn = Number(document.getElementById("offsetD").value);
-  let messageIn = document.getElementById("messageD").value;
-  let messageDec = window.cipher.decode(offsetIn, messageIn);
-  document.getElementById('outputD').innerHTML = messageDec;
+     let offsetIn = Number(document.getElementById("offsetC").value);
+     let messageIn = document.getElementById("messageC").value;
+     let messageCod = window.cipher.encode(offsetIn, messageIn);
+     document.getElementById('outputE').innerHTML = messageCod;
 
   }
-  document.getElementById('decode').addEventListener('click', decodeArg);
+
+     document.getElementById('encode').addEventListener('click', encodeArg);
+
+  function decodeArg(){
+
+     let offsetIn = Number(document.getElementById("offsetD").value);
+     let messageIn = document.getElementById("messageD").value;
+     let messageDec = window.cipher.decode(offsetIn, messageIn);
+     document.getElementById('outputD').innerHTML = messageDec;
+
+  }
+
+     document.getElementById('decode').addEventListener('click', decodeArg);
