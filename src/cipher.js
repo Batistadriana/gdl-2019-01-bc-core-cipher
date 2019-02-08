ac87b7e3dfe02ae324ee4cd9eac3a00973482edf
Batistadriana <batistadriana@hotmail.com>
2019-02-08 09:57:29 -0600

    Cambio de imagenes

--- src/cipher.js
+++ src/cipher.js
@@ -2,16 +2,16 @@ window.cipher = {
 
 encode: (offset, texto)=> {
 
-    let mensaje = [];
+    let mensaje = [];    //variable para poner mi resultado.
 
-    for(let i=0; i < texto.length; i++){
-        texto[i].charCodeAt();
-        let position = ((texto[i].charCodeAt() - 65 + offset)%26 + 65);
-        let asciiString= String.fromCharCode(position);
-        mensaje.push(asciiString);
+    for(let i=0; i < texto.length; i++){  //Condicional que avanza letra por letra el largo del texto ingresado.
+        texto[i].charCodeAt();            // Enunciado que te da el numero ASCII de cada letra.
+        let position = ((texto[i].charCodeAt() - 65 + offset)%26 + 65); //Enunciado que contiene la fórmula que te da el nuevo offset
+        let asciiString= String.fromCharCode(position); //Enunciado que con tu nuevo offset te da tu nueva letra.
+        mensaje.push(asciiString);        //Enunciado que agrega las nuevas letras a un array por medio de ".push".
 
     }
-    let x = mensaje.join('');
+    let x = mensaje.join('');  //variable que agarra el array y con ".join" une todas las letras sin espacios.
     return x;
   },
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 //Funciones de los botones para cambiar pantallas
  function boton2a() {
-     document.getElementById('screen1').style.display = 'none';
-     document.getElementById('screen2a').style.display = 'block';
+     document.getElementById('screen1').style.display = 'none';  //va por tu "screen1" y hace que su "estilo de display" sea "none" o sea, que no aparezca.
+     document.getElementById('screen2a').style.display = 'block'; //va por tu "screen2a" y hace que su "estilo de display" sea "block" (de "bloque") o sea, que aparezca.
   }
-     document.getElementById('botonEncode').addEventListener('click', boton2a);
-
+     document.getElementById('botonEncode').addEventListener('click', boton2a); //va por tu botón que te manda a la página de "encode" y le agrega el ".addEventListener"
+                                                                                //que es el que te avisará cada que tu boton sea "clickeado" y te mandará a ejecutar la función.
  function boton2b(){
      document.getElementById('screen1').style.display = 'none';
      document.getElementById('screen2b').style.display = 'block';
@@ -28,12 +28,12 @@
 
 // Funciones para limpiar los campos
  function cleanA(){
-     document.getElementById('offsetC').value="";
+     document.getElementById('offsetC').value="";  //asigna un "vacío" a mi offsetC.
      document.getElementById('messageC').value="";
      document.getElementById('outputE').value="";
   }
 
-     document.getElementById('limpiarA').addEventListener('click', cleanA);
+     document.getElementById('limpiarA').addEventListener('click', cleanA); //cada que se "clickea" en mi botón "limpiarA", se ejecuta mi función "cleanA".
 
  function cleanB(){
      document.getElementById('offsetD').value="";
@@ -48,14 +48,14 @@
 //Funciones para los argumentos de cipher.js
  function encodeArg(){
 
-     let offsetIn = Number(document.getElementById('offsetC').value);
-     let messageIn = document.getElementById('messageC').value;
-     let messageCod = window.cipher.encode(offsetIn, messageIn);
-     document.getElementById('outputE').innerHTML = messageCod;
+     let offsetIn = Number(document.getElementById('offsetC').value); //Se crea una variable donde se va agregar en valor númerico lo que entre en mi caja de "offsetC" que está en HTML.
+     let messageIn = document.getElementById('messageC').value;       //Se crea una variable donde se le da valor de texto a lo que entra en "messageC" en HTML.
+     let messageCod = window.cipher.encode(offsetIn, messageIn);      //Se crea una variable donde se llama a "Encode"(que está en mi cipher.js) y mete mis argumentos (las variables que creé).
+     document.getElementById('outputE').innerHTML = messageCod;       //Se imprime en mi caja de HTML ("outputE") lo que hay en "messageCod".
 
   }
 
-     document.getElementById('encode').addEventListener('click', encodeArg);
+     document.getElementById('encode').addEventListener('click', encodeArg); //Cada que "clickeas" el botón encode, se ejecuta tu función "encodeArg".
 
   function decodeArg(){
 
